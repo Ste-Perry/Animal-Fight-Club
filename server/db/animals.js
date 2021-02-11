555d73e223f1258a6fa5789d893eb3b822ae2117
Samardeep90 <73861988+Samardeep90@users.noreply.github.com>
2021-02-12 10:12:53 +1300

    done add animal

--- server/db/animals.js
+++ server/db/animals.js
@@ -4,6 +4,23 @@ function getAnimals (db = connection) {
   return db('animals').select()
 }
 
+function getAnimalByName (name, db = connection) {
+  return db('animals') 
+  .where('name', name)
+  .select()
+  .first()
+}
+
+function addAnimal (animal, db = connection) {
+  return db('animals')
+  .insert(animal)
+  .then(ids => ids[0])
+}
+
+
+
 module.exports = {
-  getAnimals
+  getAnimals,
+  getAnimalByName,
+  addAnimal
 }
